c1146188a0bc9edec472fb69076243db66e54ffe
= <b.scheunemann@laposte.net>
2022-05-04 19:06:05 +0200

    test

--- Page-web/app.py
+++ Page-web/app.py
@@ -5,38 +5,49 @@ from oauth2client.service_account import ServiceAccountCredentials
 import datetime as dt
 import base64
 from io import BytesIO
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+import datetime
+from googleapiclient.http import MediaInMemoryUpload
+import pickle
+import os
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import Flow, InstalledAppFlow
+from googleapiclient.discovery import build
 
-gauth = GoogleAuth()
-# Try to load saved client credentials
-gauth.LoadCredentialsFile("mycreds.txt")
 
-if gauth.credentials is None:
-    # Authenticate if they're not there
+def Create_Service(client_secret_file, api_name, api_version, *scopes):
+    print(client_secret_file, api_name, api_version, scopes, sep='-')
+    CLIENT_SECRET_FILE = client_secret_file
+    API_SERVICE_NAME = api_name
+    API_VERSION = api_version
+    SCOPES = [scope for scope in scopes[0]]
 
-    # This is what solved the issues:
-    gauth.GetFlow()
-    gauth.flow.params.update({'access_type': 'offline'})
-    gauth.flow.params.update({'approval_prompt': 'force'})
+    cred = None
 
-    gauth.LocalWebserverAuth()
+    pickle_file = f'token_{API_SERVICE_NAME}_{API_VERSION}.pickle'
 
-elif gauth.access_token_expired:
+    if os.path.exists(pickle_file):
+        with open(pickle_file, 'rb') as token:
+            cred = pickle.load(token)
 
-    # Refresh them if expired
+    if not cred or not cred.valid:
+        if cred and cred.expired and cred.refresh_token:
+            cred.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+            cred = flow.run_local_server()
 
-    gauth.Refresh()
-else:
+        with open(pickle_file, 'wb') as token:
+            pickle.dump(cred, token)
 
-    # Initialize the saved creds
+    try:
+        service = build(API_SERVICE_NAME, API_VERSION, credentials=cred)
+        print(API_SERVICE_NAME, 'service created successfully')
+        return service
+    except Exception as e:
+        print(e)
+    return None
 
-    gauth.Authorize()
 
-# Save the current credentials to a file
-gauth.SaveCredentialsFile("mycreds.txt")
-
-drive = GoogleDrive(gauth)
 images_file_id = '1J6Cbzo3L4ZELWl-I4cQxOH93nqGSmvA5'
 
 Nombre_velo = 10
@@ -49,6 +60,19 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('secret_sheet.json', sc
 client = gspread.authorize(creds)
 
 sheet = client.open('SDP_Test').sheet1
+
+# Save the current credentials to a file
+Client_secret = "client_secrets.json"
+API_NAME = 'drive'
+API_VERSION = 'v3'
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+
+service = Create_Service(Client_secret, API_NAME, API_VERSION, SCOPES)
+
+
+def convert_to_RFC_datetime(year=1900, month=1, day=1, hour=0, minute=0):
+    dt = datetime.datetime(year, month, day, hour, minute, 0).isoformat() + 'Z'
+    return dt
 
 
 def insertion(reservation):
@@ -104,10 +128,19 @@ def render_picture(data):
 
 def ajout_photo(img, img_name):
 
-    gfile = drive.CreateFile({'parents': [{'id': images_file_id}], 'title': img_name})
-    # Read file and set it as the content of this instance.
-    gfile.SetContentString(img)
-    gfile.Upload()  # Upload the file.
+    file_name = img_name
+
+    file_meta = {
+        'name': file_name,
+        'parents': [images_file_id]
+    }
+
+    media = MediaInMemoryUpload(img)
+    service.files().create(
+        body=file_meta,
+        media_body=media,
+        fields='id'
+    ).execute()
 
 
 app = Flask(__name__)
@@ -157,14 +190,12 @@ def rendre_velo():
         user_date = request.form["dt"]
         image = request.files["image"]
         data = image.read()
-        data = render_picture(data)
-        im = BytesIO(base64.b64decode(data))
+        im = bytearray(data)
         img_name = str(user_name) + '_' + str(user_date) + '.png'
-
         reservation = [str(user_name).lower(), str(user_pname).lower(), str(user_date)]
         supp = ejection(reservation)
         if supp:
-            ajout_photo(str(im.getvalue()), img_name)
+            ajout_photo(im, img_name)
             return validation_suppression()
 
         else:
